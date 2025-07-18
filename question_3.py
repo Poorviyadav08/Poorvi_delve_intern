248f23910e9569ed04654998e90289f309e362e7
Poorviyadav08 <poorvi.yadav@tigeranalytics.com>
2025-07-18 12:27:37 +0530

    Delete question_3.py

--- question_3.py
+++
@@ -1,7 +0,0 @@
-user_input = input("Enter a sequence of whitespace separated words:")
-words = user_input.split()
-unique_words = list(dict.fromkeys(words))
-sorted_output = " ".join(sorted(unique_words))
-print(sorted_output)
-
- #hello world and practice makes perfect and hello world again
