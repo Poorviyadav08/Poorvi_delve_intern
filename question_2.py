ce9fec3cd2a7c91e4567a2d7d8d80216501b2f2f
Poorviyadav08 <poorvi.yadav@tigeranalytics.com>
2025-07-18 12:29:52 +0530

    Delete question_2.py

--- question_2.py
+++
@@ -1,6 +0,0 @@
-user_input = input("Enter comma separated sequence of words as input:")
-words = user_input.split(",")
-
-# seperator = ","
-sorted_output = ",".join(sorted(words))
-print(sorted_output)
